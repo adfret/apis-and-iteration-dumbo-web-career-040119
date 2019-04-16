bcd55b63038587861287e2d6dc9f850d11569536
Ad Friedman <adien330@yahoo.com>
2019-04-16 20:07:52 +0000

    Automatically backed up by Learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,9 +3,7 @@ require 'colorize'
 require 'rest_client'
 require 'json'
 
-#Method definitions are temporary
 
-# puts "Welcome to book searcher!".magenta
 puts "What character you looking for?".green
 v = gets.chomp
 
@@ -22,12 +20,31 @@ binding.pry
  end
 end
 
+#Method definitions are temporary
 
-url = "https://www.swapi.co/api/films/"
-
-response = RestClient.get(url)
-hashes = JSON.parse response.body
-
-named_hashes.each do |i|
-  if i == hashes[]
-# named_films = hashes["title"].select do |film|
+# puts "Welcome to book searcher!".magenta
+# puts "What character you looking for?".green
+# v = gets.chomp
+#
+# url = "http://www.swapi.co/api/people/"
+#
+# response = RestClient.get(url)
+# hashes = JSON.parse response.body
+#
+# named_hashes = hashes["results"].find do |name|
+#  #IS NAME KEY EQ TO NAME GIVEN? (IS W/IN ARRAY)
+#  if v == name["name"]
+# binding.pry
+#    name
+#  end
+# end
+#
+#
+# url = "https://www.swapi.co/api/films/"
+#
+# response = RestClient.get(url)
+# hashes = JSON.parse response.body
+#
+# named_hashes.each do |i|
+#   if i == hashes[]
+# # named_films = hashes["title"].select do |film|
